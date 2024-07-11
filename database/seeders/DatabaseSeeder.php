08350adf4bd4f0be1d9cf1f70a60a433b22a3355
2003parvshah <2003parv@gmail.com>
2024-07-11 21:30:19 +0530

    db-created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,10 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('roles')->insert([
+            'name' => 'Administrator',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Student',
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'Teacher',
+        ]);
     }
 }
